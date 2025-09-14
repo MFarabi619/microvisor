d3035cd935ff19d3fe09287cf1a1558289544585
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-14 05:56:37 -0400

    build(dev/microvisor/nixos-unified): add input
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- flake.nix
+++ flake.nix
@@ -3,13 +3,6 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-  };
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+    nixos-unified.url = "github:srid/nixos-unified";
   };
 }

b1266144f36b1142b78ae0baa20b74fdad4b0cf3
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-14 05:57:39 -0400

    build(dev/microvisor/lix-module): add input
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- flake.nix
+++ flake.nix
@@ -5,5 +5,10 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-unified.url = "github:srid/nixos-unified";
     flake-parts.url = "github:hercules-ci/flake-parts";
+
+    lix-module = {
+      url = "https://git.lix.systems/lix-project/nixos-module/archive/2.93.3-1.tar.gz";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 }

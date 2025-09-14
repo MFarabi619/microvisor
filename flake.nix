44fe2980fedd169b41ef52a308fcb341056a31c1
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-14 05:56:52 -0400

    build(dev/microvisor/flake-parts): add input
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

--- flake.nix
+++ flake.nix
@@ -4,5 +4,6 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     nixos-unified.url = "github:srid/nixos-unified";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 }

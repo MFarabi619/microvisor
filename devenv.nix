ff00546e5c509d4c70a289e0f8b560c7da4773ab
Mumtahin Farabi <mfarabi619@gmail.com>
2025-09-14 05:45:49 -0400

    feat(devenv): scaffold setup
    Signed-off-by: Mumtahin Farabi <mfarabi619@gmail.com>

---
+++ devenv.nix
@@ -0,0 +1,37 @@
+{
+  pkgs,
+  lib,
+  config,
+  inputs,
+  ...
+}:
+
+{
+  # packages = with pkgs; [ ];
+
+  languages = {
+    go.enable = true;
+  };
+
+  scripts = {
+    hello.exec = ''
+      echo hello
+    '';
+  };
+
+  enterShell = ''
+    hello
+  '';
+
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+  '';
+
+  # git-hooks.hooks.shellcheck.enable = true;
+}
